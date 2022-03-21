f10cb18b87ef38ce43828d4cea7af7d1f8c579a7
Saleh Zaidan <salehzaidan64@gmail.com>
2022-03-21 17:51:13 +0700

    feat: add active nav style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,32 @@
 import { Icon } from '@iconify/react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+// Store nav data
+const navListData = [
+  { label: 'Beranda', href: '/', icon: 'bi:house-door-fill' },
+  { label: 'Harian', href: '/daily', icon: 'bi:calendar-day-fill' },
+  { label: 'Bulanan', href: '/monthly', icon: 'bi:calendar-month-fill' },
+];
 
 function Layout({ children }: React.PropsWithChildren<{}>) {
+  const router = useRouter();
+  // Render nav
+  const navList = navListData.map(navItem => (
+    <Link href={navItem.href} key={navItem.href}>
+      <a
+        className={`${
+          router.pathname === navItem.href // style active route
+            ? 'border-stone-200 bg-stone-100'
+            : 'border-transparent'
+        } flex items-center gap-3 rounded-sm border py-1.5 px-3 font-medium`}
+      >
+        <Icon icon={navItem.icon} className="h-5 w-5" />
+        {navItem.label}
+      </a>
+    </Link>
+  ));
+
   return (
     <>
       <header className="flex items-center border-b border-b-stone-200 bg-stone-50 px-6 py-3 pl-32">
@@ -12,26 +37,7 @@ function Layout({ children }: React.PropsWithChildren<{}>) {
           </h1>
           <div className="font-medium">Institut Teknologi Bandung</div>
         </div>
-        <nav className="ml-20 flex gap-8 text-blue-900">
-          <Link href="/">
-            <a className="flex items-center gap-3 font-medium">
-              <Icon icon="bi:house-door-fill" className="h-6 w-6" />
-              Beranda
-            </a>
-          </Link>
-          <Link href="/daily">
-            <a className="flex items-center gap-3 font-medium">
-              <Icon icon="bi:calendar-day-fill" className="h-6 w-6" />
-              Harian
-            </a>
-          </Link>
-          <Link href="/monthly">
-            <a className="flex items-center gap-3 font-medium">
-              <Icon icon="bi:calendar-month-fill" className="h-6 w-6" />
-              Bulanan
-            </a>
-          </Link>
-        </nav>
+        <nav className="ml-20 flex gap-8 text-blue-900">{navList}</nav>
       </header>
       <main>{children}</main>
     </>
