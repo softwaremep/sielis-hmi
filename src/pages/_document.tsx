e5965726ccef170ab82e5f261eab538ce0e63192
Saleh Zaidan <salehzaidan64@gmail.com>
2022-03-21 20:05:56 +0700

    chore: change html lang to id

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+function MyDocument() {
+  return (
+    <Html lang="id">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default MyDocument;
