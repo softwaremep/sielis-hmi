9581ae2b329920608d384ad2efeb8c41d35cdde6
Saleh Zaidan <salehzaidan64@gmail.com>
2022-03-21 13:20:33 +0700

    chore: add prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <h1>Sielis HMI</h1>
-  )
-}
+  return <h1>Sielis HMI</h1>;
+};
 
-export default Home
+export default Home;
